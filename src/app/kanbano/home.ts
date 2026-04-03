bffbf5bdbbc6acad3a4bdfb91c95f92ea14a9ce9
Sebastienpanda <sebastienprodev@gmail.com>
2026-04-03 16:29:42 +0200

    feat: add section hero-banner to homepage

--- src/app/kanbano/home.ts
+++ src/app/kanbano/home.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { Header } from './components/layout/header';
+import { HeroBanner } from './hero-banner/hero-banner';
 
 @Component({
     selector: 'kanbano-lp-home',
     imports: [
-        Header
+        Header,
+        HeroBanner
     ],
     templateUrl: './home.html',
 })
