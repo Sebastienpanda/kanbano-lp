d12cdc4b2e2da083e5459a546c366c160f5028d6
Sebastienpanda <sebastienprodev@gmail.com>
2026-04-04 08:57:49 +0200

    feat: add ssr and replace express to fastify

---
+++ src/server.ts
@@ -0,0 +1,38 @@
+import { CommonEngine, } from '@angular/ssr/node';
+import { join } from 'node:path';
+import Fastify, { FastifyInstance } from 'fastify';
+import fastifyStatic from '@fastify/static';
+import bootstrap from './main.server';
+import { APP_BASE_HREF } from '@angular/common';
+
+const browserDistFolder = join(import.meta.dirname, '../browser');
+
+export async function createServer(): Promise<FastifyInstance> {
+    const app = Fastify({logger: true});
+    const engine = new CommonEngine();
+
+    app.register(fastifyStatic, {
+        root: browserDistFolder,
+        wildcard: false,
+    });
+
+    app.get('*', async (req, reply) => {
+        const html = await engine.render({
+            bootstrap,
+            documentFilePath: join(browserDistFolder, 'index.html'),
+            url: req.url,
+            publicPath: browserDistFolder,
+            providers: [{provide: APP_BASE_HREF, useValue: req.url}],
+        });
+
+        reply.type('text/html').send(html);
+    });
+
+    return app;
+}
+
+const port = process.env['PORT'] ? +process.env['PORT'] : 4000;
+createServer().then((app) => {
+    void app.listen({port, host: '0.0.0.0'});
+});
+
