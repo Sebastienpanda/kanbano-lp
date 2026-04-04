4f51b15ec5f9e0a6d3c34f2ef879121205260f29
Sebastienpanda <sebastienprodev@gmail.com>
2026-04-04 09:12:56 +0200

    feat: new section preview and add preview component for the homepage and add differently images

--- src/app/kanbano/home.ts
+++ src/app/kanbano/home.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { Header } from './components/layout/header';
 import { HeroBanner } from './hero-banner/hero-banner';
+import { Preview } from './preview/preview';
 
 @Component({
     selector: 'kanbano-lp-home',
     imports: [
         Header,
-        HeroBanner
+        HeroBanner,
+        Preview
     ],
     templateUrl: './home.html',
 })
---
+++ src/app/kanbano/preview/preview.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+
+@Component({
+    selector: 'kanbano-lp-preview',
+    imports: [
+        NgOptimizedImage
+    ],
+    templateUrl: './preview.html',
+    styleUrl: './preview.css',
+})
+export class Preview {
+
+}
