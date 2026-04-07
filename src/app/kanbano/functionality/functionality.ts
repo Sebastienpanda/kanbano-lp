e6d1e668256890ec276dd57c39dc57aada892b3e
Sebastienpanda <sebastienprodev@gmail.com>
2026-04-07 11:48:21 +0200

    chore: update functionality and add differently formats imgs

--- src/app/kanbano/functionality/functionality.ts
+++ src/app/kanbano/functionality/functionality.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 import { FunctionalityIcon } from './icons/functionality-icon';
-import { VectorGreenTop } from './svg/vector-green-top';
-import { VectorPurpleTop2 } from './svg/vector-purple-top-2';
+import { Button } from '../components/utilities/button';
 
 @Component({
     selector: 'kanbano-lp-functionality',
     imports: [
         FunctionalityIcon,
-        VectorGreenTop,
-        VectorPurpleTop2
+        Button,
     ],
     templateUrl: './functionality.html',
     styleUrl: './functionality.css',
