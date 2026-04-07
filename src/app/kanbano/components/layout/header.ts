dc645f5a1743ed4f439459543a4e21ad9cba9984
Sebastienpanda <sebastienprodev@gmail.com>
2026-04-07 14:55:48 +0200

    feat: add effect glassmorphism to scroll

--- src/app/kanbano/components/layout/header.ts
+++ src/app/kanbano/components/layout/header.ts
@@ -1,14 +1,36 @@
-import { Component } from '@angular/core';
+import {
+    afterNextRender,
+    ChangeDetectionStrategy,
+    Component,
+    DOCUMENT,
+    inject,
+    OnDestroy,
+    signal
+} from '@angular/core';
 import { Logo } from '../../shared/icons/logo';
 
 @Component({
     selector: 'kanbano-lp-header',
-    imports: [
-        Logo
-    ],
+    imports: [Logo],
     templateUrl: './header.html',
     styleUrl: './header.css',
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Header {
+export class Header implements OnDestroy {
+    scrolled = signal(false);
+    private document = inject(DOCUMENT);
 
+    constructor() {
+        afterNextRender(() => {
+            this.document.defaultView?.addEventListener('scroll', this.onScroll, {passive: true});
+        });
+    }
+
+    ngOnDestroy() {
+        this.document.defaultView?.removeEventListener('scroll', this.onScroll);
+    }
+
+    private onScroll = () => {
+        this.scrolled.set((this.document.defaultView?.scrollY ?? 0) > 10);
+    };
 }
