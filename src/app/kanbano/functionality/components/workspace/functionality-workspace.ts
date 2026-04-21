80d0f85294002875488d095239f20fe9481abe41
Sebastienpanda <sebastienprodev@gmail.com>
2026-04-21 14:49:45 +0200

    chore: big update section

--- src/app/kanbano/functionality/components/workspace/functionality-workspace.ts
+++ src/app/kanbano/functionality/components/workspace/functionality-workspace.ts
@@ -4,9 +4,9 @@ import { VectorBlueBottom } from "../../svg/vector-blue/vector-blue-bottom";
 import { VectorGreenTop } from "../../svg/vector-green/vector-green-top";
 import { VectorPurpleTop2 } from "../../svg/vector-purple/vector-purple-top-2";
 import { HeadingItem } from "../heading-item";
-import { WorkspaceCreateSvg } from "./components/create/workspace-create-svg";
-import { WorkspaceSvg } from "./components/empty/workspace-svg";
-import { WorkspaceViewSvg } from "./components/view/workspace-view-svg";
+import { WorkspaceCreate } from "./components/create/workspace-create";
+import { WorkspaceEmpty } from "./components/empty/workspace-empty";
+import { WorkspaceView } from "./components/view/workspace-view";
 
 @Component({
     selector: "kanbano-lp-functionality-workspace",
@@ -15,9 +15,9 @@ import { WorkspaceViewSvg } from "./components/view/workspace-view-svg";
         VectorBlueBottom,
         VectorGreenTop,
         VectorPurpleTop2,
-        WorkspaceSvg,
-        WorkspaceCreateSvg,
-        WorkspaceViewSvg,
+        WorkspaceEmpty,
+        WorkspaceCreate,
+        WorkspaceView,
     ],
     templateUrl: "./functionality-workspace.html",
     styleUrl: "./functionality-workspace.css",
