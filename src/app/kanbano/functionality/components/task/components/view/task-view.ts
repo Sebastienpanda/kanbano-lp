559d977bc11eb30f5e08df808e596399f1935d1e
Sebastienpanda <sebastienprodev@gmail.com>
2026-04-21 15:22:24 +0200

    chore: remove xxxxx-svg to all files

---
+++ src/app/kanbano/functionality/components/task/components/view/task-view.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "kanbano-lp-view",
+    imports: [],
+    templateUrl: "./task-columns-view-svg.html",
+    styleUrl: "./task-columns-view-svg.css",
+})
+export class TaskColumnsViewSvg {}
