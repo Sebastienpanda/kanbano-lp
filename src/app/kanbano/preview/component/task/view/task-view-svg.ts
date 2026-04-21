b7b3a66ba5df7b340d84f5c7d5ac43724ac1f488
Sebastienpanda <sebastienprodev@gmail.com>
2026-04-21 11:52:17 +0200

    chore: update section preview and remove svg

--- src/app/kanbano/preview/component/task/view/task-view-svg.ts
+++ src/app/kanbano/preview/component/task/view/task-view-svg.ts
@@ -1,19 +1,10 @@
-import { BreakpointObserver } from "@angular/cdk/layout";
-import { ChangeDetectionStrategy, Component, inject } from "@angular/core";
-import { toSignal } from "@angular/core/rxjs-interop";
-import { map } from "rxjs";
+import { Component } from "@angular/core";
 
 @Component({
     selector: "kanbano-lp-task-view-svg",
     imports: [],
     templateUrl: "./task-view-svg.html",
     styleUrl: "./task-view-svg.css",
-    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TaskViewSvg {
-    protected readonly breakpoint = inject(BreakpointObserver);
-
-    isTablet = toSignal(this.breakpoint.observe("(min-width: 768px)").pipe(map((r) => r.matches)), {
-        initialValue: false,
-    });
 }
