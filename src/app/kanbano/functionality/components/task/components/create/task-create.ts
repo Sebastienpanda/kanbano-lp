e9db37f0d70083ef22715b5c72da3ca06c6ff4c7
Sebastienpanda <sebastienprodev@gmail.com>
2026-04-22 12:44:40 +0200

    chore: update arrow css

--- src/app/kanbano/functionality/components/task/components/create/task-create.ts
+++ src/app/kanbano/functionality/components/task/components/create/task-create.ts
@@ -1,12 +1,9 @@
 import { Component } from "@angular/core";
-import { ArrowOneTaskLaptop } from "@kanbano/functionality/components/task/components/icon/arrow/one/laptop/arrow-one-task-laptop";
-import { ArrowOneTask } from "@kanbano/functionality/components/task/components/icon/arrow/one/mobile/arrow-one-task";
 import { ArrowThreeTask } from "@kanbano/functionality/components/task/components/icon/arrow/three/arrow-three-task";
-import { ArrowTwoTask } from "@kanbano/functionality/components/task/components/icon/arrow/two/arrow-two-task";
 
 @Component({
     selector: "kanbano-lp-task-create",
-    imports: [ArrowOneTask, ArrowOneTaskLaptop, ArrowTwoTask, ArrowThreeTask],
+    imports: [ArrowThreeTask],
     templateUrl: "./task-create.html",
     styleUrl: "./task-create.css",
 })
--- src/app/kanbano/functionality/components/task/components/empty/task-empty.ts
+++ src/app/kanbano/functionality/components/task/components/empty/task-empty.ts
@@ -1,8 +1,11 @@
 import { Component } from "@angular/core";
+import { ArrowOneTaskLaptop } from "@kanbano/functionality/components/task/components/icon/arrow/one/laptop/arrow-one-task-laptop";
+import { ArrowOneTask } from "@kanbano/functionality/components/task/components/icon/arrow/one/mobile/arrow-one-task";
+import { ArrowTwoTask } from "@kanbano/functionality/components/task/components/icon/arrow/two/arrow-two-task";
 
 @Component({
     selector: "kanbano-lp-task-empty",
-    imports: [],
+    imports: [ArrowOneTask, ArrowOneTaskLaptop, ArrowTwoTask],
     templateUrl: "./task-empty.html",
     styleUrl: "./task-empty.css",
 })
