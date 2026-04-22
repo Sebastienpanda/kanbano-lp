8bf4eaf099528948ec2b48aae97afb14d2be96d1
Sebastienpanda <sebastienprodev@gmail.com>
2026-04-22 16:04:52 +0200

    chore: update arrow css

---
+++ src/app/kanbano/functionality/components/participants/components/icons/arrow/desktop/one/arrow-one-desktop.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'kanbano-lp-arrow-one-desktop',
+  imports: [],
+  templateUrl: './arrow-one-desktop.html',
+  styleUrl: './arrow-one-desktop.css',
+})
+export class ArrowOneDesktop {
+
+}
---
+++ src/app/kanbano/functionality/components/participants/components/icons/arrow/mobile/one/arrow-one-mobile.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'kanbano-lp-arrow-mobile',
+  imports: [],
+  templateUrl: './arrow-one-mobile.html',
+  styleUrl: './arrow-one-mobile.css',
+})
+export class ArrowOneMobile {
+
+}
