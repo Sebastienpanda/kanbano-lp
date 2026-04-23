445fbc1cc93a5d228a8ed0ea0b9e3c109c5397a1
Sebastienpanda <sebastienprodev@gmail.com>
2026-04-23 18:32:35 +0200

    feat: add section price

--- src/app/kanbano/home.ts
+++ src/app/kanbano/home.ts
@@ -1,13 +1,14 @@
 import { Component } from "@angular/core";
+import { Header } from "@components/layout/header";
+import { Pricing } from "@kanbano/pricing/pricing";
 
-import { Header } from "./components/layout/header";
 import { Functionality } from "./functionality/functionality";
 import { HeroBanner } from "./hero-banner/hero-banner";
 import { Preview } from "./preview/preview";
 
 @Component({
     selector: "kanbano-lp-home",
-    imports: [Header, HeroBanner, Preview, Functionality],
+    imports: [Header, HeroBanner, Preview, Functionality, Pricing],
     templateUrl: "./home.html",
 })
 export class Home {}
