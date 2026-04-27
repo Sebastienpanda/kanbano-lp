8996470ee20a244ef208cc64f8c5b67800b77334
Sebastienpanda <sebastienprodev@gmail.com>
2026-04-27 22:13:44 +0200

    chore: update logo with differently sections

--- src/app/kanbano/components/layout/section-header.ts
+++ src/app/kanbano/components/layout/section-header.ts
@@ -8,6 +8,5 @@ import { ChangeDetectionStrategy, Component, input } from "@angular/core";
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SectionHeader {
-    readonly icon = input.required<string>();
     readonly title = input.required<string>();
 }
