913a66133e7147fd456e0349af51f2e2d2ea135a
Sebastienpanda <sebastienprodev@gmail.com>
2026-04-28 22:11:53 +0200

    chore: remove unnecessary imports

--- src/app/kanbano/functionality/functionality.ts
+++ src/app/kanbano/functionality/functionality.ts
@@ -4,11 +4,10 @@ import { CtaActions } from "@components/utilities/cta-actions";
 import { FunctionalityParticipants } from "./components/participants/functionality-participants";
 import { FunctionalityTask } from "./components/task/functionality-task";
 import { FunctionalityWorkspace } from "./components/workspace/functionality-workspace";
-import { FunctionalityIcon } from "./icons/functionality-icon";
 
 @Component({
     selector: "kanbano-lp-functionality",
-    imports: [FunctionalityIcon, CtaActions, FunctionalityWorkspace, FunctionalityTask, FunctionalityParticipants],
+    imports: [CtaActions, FunctionalityWorkspace, FunctionalityTask, FunctionalityParticipants],
     templateUrl: "./functionality.html",
     styleUrl: "./functionality.css",
 })
