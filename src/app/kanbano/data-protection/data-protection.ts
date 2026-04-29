20b6f9255b934eb65b5187936f7e4ddb8ad65fa5
Sebastienpanda <sebastienprodev@gmail.com>
2026-04-29 11:34:01 +0200

    feat: new section data protection

--- src/app/kanbano/data-protection/data-protection.ts
+++ src/app/kanbano/data-protection/data-protection.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
 import { SectionHeader } from "@components/layout/section-header";
+import { LucideArrowRight } from "@lucide/angular";
 
 @Component({
     selector: "kanbano-lp-data-protection",
-    imports: [SectionHeader],
+    imports: [SectionHeader, LucideArrowRight],
     templateUrl: "./data-protection.html",
     styleUrl: "./data-protection.css",
     changeDetection: ChangeDetectionStrategy.OnPush,
